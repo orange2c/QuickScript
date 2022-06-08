8aacb01e7fb3c2b1c9adc9ce8a4704d70a1a655a
orange2c <3335447573@qq.com>
2022-06-08 15:55:12 +0800

    初次编写nginx-install

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # QuickScript
 实现各种快捷功能的shell脚本
+
+
+## nginx-install.sh 
+	ubuntu自动下载nginx，创建/Ndownload文件夹，配置此路径为文件服务器的脚本路径
+
---
+++ nginx-install.sh
@@ -0,0 +1,28 @@
+
+echo "本脚本用于安装配置nginx为文件服务器，并设置路径为/Ndownload"
+read -p '按下回车键开始'
+
+echo "1.安装nginx"
+apt-get install nginx -y
+
+echo "2.创建路径 /Ndownload "
+mkdir /Ndownload
+
+echo "3.创建配置文件"
+cat>/etc/nginx/conf.d/file_server.conf<<EOF
+server {
+    listen  80;          # 监听端口
+    server_name    _; 
+    charset utf-8; # 避免中文乱码
+    root /Ndownload;    # 文件路径
+    location / {
+        autoindex on; # 索引
+        autoindex_exact_size on; # 显示文件大小
+        autoindex_localtime on; # 显示文件时间
+    }
+}
+EOF
+
+rm /etc/nginx/sites-enabled/default
+service nginx reload
+sudo ufw allow 'Nginx Full'
